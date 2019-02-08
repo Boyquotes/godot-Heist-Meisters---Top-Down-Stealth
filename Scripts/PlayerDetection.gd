652e985af732312e1b478dc065fc92dca09fb45d
Nannings <mrnannings@gmail.com>
2019-02-08 16:30:55 +0100

    11;

--- Scripts/PlayerDetection.gd
+++ Scripts/PlayerDetection.gd
@@ -1,13 +1,14 @@
 extends "res://Scripts/Character.gd"
 
-const FOV_TOLERANCE = 22
+const FOV_TOLERANCE = 20
+const MAX_DETECTION_RANGE = 320
 const RED = Color(1, .25, .25)
 const WHITE = Color(1, 1, 1)
 
 onready var Player = get_node("/root/Level1/Player")
 
 func _process(delta):
-	if Player_is_in_FOV_TOLERANCE():
+	if Player_is_in_FOV_TOLERANCE() and Player_is_in_LOS():
 		$Torch.color = RED
 	else:
 		$Torch.color = WHITE
@@ -17,6 +18,18 @@ func Player_is_in_FOV_TOLERANCE():
 	var direction_to_Player = (Player.global_position - global_position).normalized()
 	
 	if abs(direction_to_Player.angle_to(NPC_facing_direction)) < deg2rad(FOV_TOLERANCE):
+		return true
+	else:
+		return false
+
+func Player_is_in_LOS():
+	var space = get_world_2d().direct_space_state
+	var LOS_obstacle = space.intersect_ray(global_position, Player.global_position, [self], collision_mask)
+	
+	var distance_to_player = Player.global_position.distance_to(global_position)
+	var player_in_range = distance_to_player < MAX_DETECTION_RANGE
+	
+	if LOS_obstacle and LOS_obstacle.collider == Player and player_in_range:
 		return true
 	else:
 		return false
