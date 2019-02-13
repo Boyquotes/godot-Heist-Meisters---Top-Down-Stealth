6d8d61fc5417625c8988a7fd0531fe7f5078b09b
Nannings <mrnannings@gmail.com>
2019-02-13 14:45:32 +0100

    up;

--- Scripts/Character.gd
+++ Scripts/Character.gd
@@ -3,4 +3,3 @@ extends KinematicBody2D
 const SPEED = 100
 const MAX_SPEED = 600
 const FRICTION = 0.3
-
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,13 +1,16 @@
 extends "res://Scripts/Character.gd"
 
-var motion = Vector2()
+var motion =Vector2()
+
 
 func _ready():
 	Global.Player = self
 
+
 func _process(delta):
 	update_motion(delta)
 	move_and_slide(motion)
+
 
 func update_motion(delta):
 	look_at(get_global_mouse_position())
@@ -18,7 +21,7 @@ func update_motion(delta):
 		motion.y = clamp((motion.y + SPEED), 0, MAX_SPEED)
 	else:
 		motion.y = lerp(motion.y, 0, FRICTION)
-		
+	
 	if Input.is_action_pressed("ui_left") and not Input.is_action_pressed("ui_right"):
 		motion.x = clamp((motion.x - SPEED), -MAX_SPEED, 0)
 	elif Input.is_action_pressed("ui_right") and not Input.is_action_pressed("ui_left"):
@@ -26,13 +29,4 @@ func update_motion(delta):
 	else:
 		motion.x = lerp(motion.x, 0, FRICTION)
 
-#func _input(event):
-#	if Input.is_action_just_pressed("ui_select"):
-#		toggle_torch()
-#
-#func toggle_torch():
-#	if $Torch.enabled:
-#		$Torch.enabled = false
-#	else:
-#		$Torch.enabled = true
-#
+
