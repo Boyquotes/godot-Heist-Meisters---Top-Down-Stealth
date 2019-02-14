91ba839f61c4bb6517db1165940d7ccea17b3652
Nannings <mrnannings@gmail.com>
2019-02-14 22:20:20 +0100

    19;

--- Scripts/VisionMode.gd
+++ Scripts/VisionMode.gd
@@ -8,12 +8,16 @@ func _ready():
 	color = DARK
 
 func NightVision_mode():
+	inform_npcs("NightVision_mode")
 	color = NIGHTVISION
 	$AudioStreamPlayer.stream = load(Global.nightvision_on_sfx)
 	$AudioStreamPlayer.play()
 
 func DarkVision_mode():
+	inform_npcs("DarkVision_mode")
 	color = DARK
 	$AudioStreamPlayer.stream = load(Global.nightvision_off_sfx)
 	$AudioStreamPlayer.play()
 
+func inform_npcs(vision_mode):
+	get_tree().call_group("npc", vision_mode)
